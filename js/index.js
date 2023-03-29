2d115605ad5a7fb3139f0ca537ec7a6a7e3e3fe0
Hxmmer <126730841+Hxmmer@users.noreply.github.com>
2023-03-29 16:19:42 +0100

    004-cleanup_data

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var removeCartItem = document.getElementsByClassName('btn btn-primary btn-sm remove')
+console.log(removeCartItem);
+for (var i = 0; i < removeCartItem.length; i++) {
+    var button = removeCartItem[i]
+    button.addEventListener('click', function () {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    })
+}
